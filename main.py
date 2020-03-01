a2bf516673999589be7a501a322dad9d20b55a29
DyeffersonAz <dyeffersonazevedo@hotmail.com>
2020-03-01 12:59:06 -0300

    Changes according convention

--- main.py
+++ main.py
@@ -2,23 +2,20 @@
 
 import pygame
 
-from player import *
+from player import Player
 
 pygame.init()
 
+#Creating the main window and setting caption
 SCREEN = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Falling Things Game")
 
 #Setting Images
 ground_img = pygame.image.load("ground.png")
-def draw_player(x, y):
-    pygame.draw.rect(SCREEN, (0, 0, 0), pygame.Rect((x, y), (50, 50)))
 
 #Player Variables
 player = Player(400, 450, 0)
-player_x = 400
-player_x_acc = 0
-player_y = 450
+
 
 running = True
 #Game Loop
@@ -30,13 +27,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             print("QUIT Event: Exiting window!")
-        
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 player.acc_x = 1
             if event.key == pygame.K_LEFT:
                 player.acc_x = -1
-        
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                 player.acc_x = 0
--- player.py
+++ player.py
@@ -1,11 +1,15 @@
+"""Defines the player and all its methods"""
+
 import pygame
 
 class Player:
+    """Defines Player"""
 
     def __init__(self, x, y, acc_x):
         self.x = x
         self.y = y
         self.acc_x = acc_x
-    
+
     def draw(self, screen):
+        """Puts the player on screen with given display object"""
         pygame.draw.rect(screen, (0, 0, 0), pygame.Rect((self.x, self.y), (50, 50)))
